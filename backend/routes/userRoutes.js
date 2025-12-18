46bfa3d5cab0085c82b6c448954da976ed5d8dd1
PApi0987 <msigma997@gmail.com>
2025-12-18 23:14:58 +0100

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,1 +1,13 @@
+// routes/userRoutes.js
+import express from 'express';
+import { registerUser, getUsers } from '../controllers/userController.js';
 
+const router = express.Router();
+
+// Register
+router.post('/register', registerUser);
+
+// List users
+router.get('/', getUsers);
+
+export default router;
