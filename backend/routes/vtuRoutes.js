5b6be2571f0ec2e1282fd0ef7afc91e384105e34
PApi0987 <msigma997@gmail.com>
2025-12-23 13:06:11 +0100

    Update vtuRoutes.js

--- backend/routes/vtuRoutes.js
+++ backend/routes/vtuRoutes.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import { purchaseData, purchaseAirtime, purchaseCable, purchaseElectricity } from '../controllers/vtuController.js';
+import express from "express";
+import { getDataPlans, getAirtimeProviders, getCablePlans, getElectricityDiscos } from "../controllers/vtuController.js";
 
 const router = express.Router();
 
-router.post('/data', purchaseData);
-router.post('/airtime', purchaseAirtime);
-router.post('/cable', purchaseCable);
-router.post('/electricity', purchaseElectricity);
+router.get("/plans/data", getDataPlans);
+router.get("/plans/airtime", getAirtimeProviders);
+router.get("/plans/cable", getCablePlans);
+router.get("/plans/electricity", getElectricityDiscos);
 
 export default router;
