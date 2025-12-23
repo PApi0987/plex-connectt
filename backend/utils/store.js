187e3cb7b3a19584bdd7b22cd83d15166d0d3266
PApi0987 <msigma997@gmail.com>
2025-12-23 12:59:47 +0100

    Refactor users array to single object format

--- backend/utils/store.js
+++ backend/utils/store.js
@@ -1,9 +1,5 @@
 export const users = [
-  {
-    id: 1,
-    name: "Test User",
-    wallet_balance: 5000
-  }
+  { id: 1, name: "Test User", wallet_balance: 5000 }
 ];
 
 export const transactions = [];
