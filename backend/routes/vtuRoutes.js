a78af707b26745b7ca2f9670e80f73f2a2a9dfc6
PApi0987 <msigma997@gmail.com>
2025-12-23 13:02:56 +0100

    Refactor import statements in vtuRoutes.js

--- backend/routes/vtuRoutes.js
+++ backend/routes/vtuRoutes.js
@@ -1,25 +1,11 @@
-// routes/vtuRoutes.js
-
 import express from 'express';
-import {
-  purchaseData,
-  purchaseAirtime,
-  purchaseCable,
-  purchaseElectricity
-} from '../controllers/vtuController.js';
+import { purchaseData, purchaseAirtime, purchaseCable, purchaseElectricity } from '../controllers/vtuController.js';
 
 const router = express.Router();
 
-// ----------------- DATA -----------------
 router.post('/data', purchaseData);
-
-// ----------------- AIRTIME -----------------
 router.post('/airtime', purchaseAirtime);
-
-// ----------------- CABLE -----------------
 router.post('/cable', purchaseCable);
-
-// ----------------- ELECTRICITY -----------------
 router.post('/electricity', purchaseElectricity);
 
 export default router;
