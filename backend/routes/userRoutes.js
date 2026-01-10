67e80a85d8349036eb62bf07604afc0a85bdcd58
PApi0987 <msigma997@gmail.com>
2026-01-10 14:11:52 +0100

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,23 +1,9 @@
-import express from 'express';
-import {
-  registerUser,
-  getUsers,
-  fundWallet,
-  getWalletBalance,
-  getUserTransactions
-} from '../controllers/userController.js';
+import express from "express";
+import { signup, login } from "../controllers/userController.js";
 
 const router = express.Router();
 
-// Users
-router.post('/register', registerUser);
-router.get('/', getUsers);
-
-// Wallet
-router.post('/wallet/fund', fundWallet);
-router.get('/wallet/:userId', getWalletBalance);
-
-// Transactions
-router.get('/transactions/:userId', getUserTransactions);
+router.post("/signup", signup);
+router.post("/login", login);
 
 export default router;
