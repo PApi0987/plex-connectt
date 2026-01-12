3f8387fe8412b3fe075bb7071484e6f77428a75c
PApi0987 <msigma997@gmail.com>
2026-01-12 21:38:38 +0100

    Create generateToken.js

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,11 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (userId, role = "user") => {
+  return jwt.sign(
+    { id: userId, role },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+};
+
+export default generateToken;
