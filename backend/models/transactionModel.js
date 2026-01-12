5a99934ee35e820737467e9915453cce411db75c
PApi0987 <msigma997@gmail.com>
2026-01-12 21:57:45 +0100

    Update transactionModel.js

--- backend/models/transactionModel.js
+++ backend/models/transactionModel.js
@@ -1,14 +1,11 @@
 import mongoose from "mongoose";
 
-const transactionSchema = new mongoose.Schema(
-  {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    type: { type: String }, // funding, airtime, data, etc
-    amount: { type: Number },
-    reference: { type: String },
-    status: { type: String, default: "success" },
-  },
-  { timestamps: true }
-);
+const transactionSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  service: { type: String, required: true },
+  amount: { type: Number, required: true },
+  status: { type: String, default: "success" },
+  date: { type: Date, default: Date.now }
+});
 
 export default mongoose.model("Transaction", transactionSchema);
