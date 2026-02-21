1f391239103e004d27fd6bcbb372fd6603f4abba
PApi0987 <msigma997@gmail.com>
2026-02-21 18:24:55 +0100

    Refactor admin routes to use adminProtect middleware
    Updated admin routes to use adminProtect middleware for user and transaction endpoints.

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,23 +1,14 @@
 import express from "express";
-import { adminAuth } from "../middlewares/adminAuth.js";
 import {
-  getAdminStats,
   getAllUsers,
-  getAllTransactions
+  getAllTransactions,
 } from "../controllers/adminController.js";
+
+import { adminProtect } from "../middlewares/adminAuth.js";
 
 const router = express.Router();
 
-// Protect all routes
-router.use(adminAuth);
-
-// Dashboard stats
-router.get("/stats", getAdminStats);
-
-// List users
-router.get("/users", getAllUsers);
-
-// List transactions
-router.get("/transactions", getAllTransactions);
+router.get("/users", adminProtect, getAllUsers);
+router.get("/transactions", adminProtect, getAllTransactions);
 
 export default router;
